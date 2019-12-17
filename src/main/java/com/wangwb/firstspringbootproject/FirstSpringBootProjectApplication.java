69eec6467a03b341200ad3482f427e7547944857
wwb <464405870@qq.com>
2019-12-17 15:15:32 +0800

    first commit

---
+++ src/main/java/com/wangwb/firstspringbootproject/FirstSpringBootProjectApplication.java
@@ -0,0 +1,20 @@
+package com.wangwb.firstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FirstSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootProjectApplication.class, args);
+    }
+    @RequestMapping
+    public String hello() {
+        return "hello spring boot!";
+    }
+}
---
+++ src/test/java/com/wangwb/firstspringbootproject/FirstSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangwb.firstspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
