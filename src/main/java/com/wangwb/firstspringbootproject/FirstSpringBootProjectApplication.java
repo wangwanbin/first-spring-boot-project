0fc1172152338a379072cb7d62c7efa76ec1d832
wangwanbin <31121084+wangwanbin@users.noreply.github.com>
2019-12-17 18:16:45 +0800

    Update FirstSpringBootProjectApplication.java

--- src/main/java/com/wangwb/firstspringbootproject/FirstSpringBootProjectApplication.java
+++ src/main/java/com/wangwb/firstspringbootproject/FirstSpringBootProjectApplication.java
@@ -15,6 +15,6 @@ public class FirstSpringBootProjectApplication {
     }
     @RequestMapping
     public String hello() {
-        return "hello spring boot! v2.0";
+        return "hello spring boot! v3.0";
     }
 }
